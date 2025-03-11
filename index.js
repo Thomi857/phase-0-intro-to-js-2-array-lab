b7a0d135b02b83b2f3b04334d5b534cac7695def
CliffLinusthomi <linusthomi2@gmail.com>
2025-03-11 12:38:37 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    const kitten = [...cats];
+    kitten.push(name);
+    return kitten;
+}
+
+function prependCat(name){
+    const kitten = [...cats];
+    kitten.unshift(name);
+    return kitten;
+}
+function removeLastCat(){
+   const  kitten = [...cats];
+   kitten.pop(name);
+   return kitten;
+}
+function removeFirstCat(){
+    const  kitten = [...cats];
+    kitten.shift(name);
+    return kitten;
+ }
